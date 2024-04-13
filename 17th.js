a2d5efd2d8e42190292a25e57fd0a7c74cf4973d
Bhanu Sharma <bhanusharma576@gmail.com>
2024-04-13 15:17:51 +0530

    older method is some mistake that's new

--- 17th.js
+++ 17th.js
@@ -3,16 +3,21 @@ Write a function to convert a string to camelCase & snake_case.
 */
 
 const toCamelCase = (str)=>{
-    str = str.toLowerCase().split(" ");
-    let newStr = str.map(word=>word[0])
-   newStr= newStr.join("").toUpperCase();
-   let Array = []
-    str.filter((curr,index)=>{
-         let bhan = newStr[index]+curr.slice(1)
-        Array.push(bhan)
+    str = str.trim().toLowerCase().split(" ");
+    
+   const newStr =  str.map((curr,index)=>{
+        if (index===0)
+        {
+            return curr
+        }else{
+         return curr.charAt(0).toUpperCase()+curr.slice(1)
+       
+        }
          
     })
-    return Array.join("")
+    return newStr.join("");
+   
+    
     
 
 }
