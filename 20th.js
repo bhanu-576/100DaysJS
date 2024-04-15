d15d1a01253afcd79fffd26ef743119f44e5d2ae
Bhanu Sharma <bhanusharma576@gmail.com>
2024-04-15 22:26:52 +0530

    done with concat

--- 20th.js
+++ 20th.js
@@ -2,15 +2,27 @@
 Write a function to reverse a string without using built-in methods or libraries. 
 The function should take a string as input and return the reversed string
 */
+ // using array
+// const reverseString = (str)=>{
+//     let reverse =[];
 
+//     for(i=str.length-1;i>=0;i--){
+//         reverse.push(str[i])
+
+//     }
+//     return reverse.join("");
+
+// }
+
+// using con cat
 const reverseString = (str)=>{
-    let reverse =[];
+    let reverse ="";
 
     for(i=str.length-1;i>=0;i--){
-        reverse.push(str[i])
+        reverse +=str[i];
 
     }
-    return reverse.join("");
+    return reverse;
 
 }
 
