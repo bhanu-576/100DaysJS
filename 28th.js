8a7f67ff4f1160c182fc7ef91e6d9deca6afc135
Bhanu Sharma <bhanusharma576@gmail.com>
2024-04-23 22:44:47 +0530

    question write

---
+++ 28th.js
@@ -0,0 +1,17 @@
+/*
+* Write a function called truncateString that takes two parameter
+ ? str: A string that needs to be truncaated.
+ ? maxLength : An integer representing the maximum length of the string allowed.
+ ? The function should truncate the input string str if its length exceeds the specified maxLength.
+ If trincate occurs,the function should add '...' to the end of the truncate string.
+
+*/
+ console.log(truncateString("A-tisket a-tasket A green and yellow basket",8));
+
+ 
+ //output "A-tisket ..."
+
+ // Constraints: 
+ // The input string str will contains only alphanumeric character and punctuacion marks.
+ // The maximum length maxlength will be aa positive integer.
+ // the output string length should not exceed maxLength +3 characters dueto the addition of "..."
