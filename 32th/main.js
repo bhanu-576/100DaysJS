d8885a0cb5c7210f0dbecffee2020200cff6890a
Bhanu Sharma <bhanusharma576@gmail.com>
2024-05-02 06:06:16 +0530

    mini project done

--- 32th/main.js
+++ 32th/main.js
@@ -1,30 +1,21 @@
-const imageUrl = [
-    './photos/1.jpg',
-    './photos/2.jpg',
-    './photos/3.jpg',
-    './photos/4.jpg',
-    './photos/5.jpg',
-    './photos/6.jpg',
-    './photos/7.jpg',
-    './photos/8.jpg',
-    './photos/9.jpg',
-    './photos/10.jpg',
-    './photos/11.jpg',
-    './photos/12.jpg',
-    './photos/13.jpg',
-    './photos/14.jpg',
-    './photos/15.jpg'];
-    const imageContainer = document.getElementsById("image");
+const gallery = document.querySelectorAll('img');
 document.getElementById('button').addEventListener("click",(e)=>{
-    imageContainer.innerHTML = '';
-    for (let arr of imageUrl) {
-        const img = document.createElement('img');
-        img.src = arr;
-        img.style.marginRight = '10px';
-        imageContainer.appendChild(img);
-    }
-if(e.target.className === 'animal'){
-   
-}
+   filterSearch(e.target.className)
 
 })
+
+function filterSearch(value){
+    gallery.forEach(curr=>{
+        if(value === 'all'){
+            curr.style.display = 'inline-block'
+        }else{
+        if(curr.className === value){
+            curr.style.display = 'inline-block';
+        } else {
+            curr.style.display = 'none';
+        }
+    }
+
+    })
+
+}
