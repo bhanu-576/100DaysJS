94a3214ddd42c24b9b9ce413f24c748d07a5fa12
Bhanu Sharma <bhanusharma576@gmail.com>
2024-05-13 23:15:45 +0530

    done 43th day

---
+++ 44th.js
@@ -0,0 +1,23 @@
+//* Programming Challenge: Password Validation Coding Problem
+
+//?Create a function validatePassword that checks if a given password string meets the following criteria:
+
+/*
+? Minimum Length: The password must be at least 8 characters long.
+? Case Reqquirements: The password must include at least one uppercase letter and at least one lowerCase letter.
+? Numerical Requirement: The password must contain at least one digit.
+? Special Character Requirement: The password must have at leat one special character from the set !#$%^&*()_+=.
+*/
+
+//* Requirements
+//? The function should return true if the password meets all the criteria and false otherwise.
+//? You are to implement this function using js.
+
+function validatePassword(password){
+    const regex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*[\d])(?=.*[\W_]).{8,}$/;
+    return regex.test(password);
+
+}
+
+console.log(validatePassword("Pass123@")); //Output true
+console.log(validatePassword("password")); //output false
