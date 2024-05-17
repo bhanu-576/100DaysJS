99579639c67d32edfec50b03db620edea27726f3
Bhanu Sharma <bhanusharma576@gmail.com>
2024-05-17 22:44:11 +0530

    method 2 is added

--- 48th.js
+++ 48th.js
@@ -11,20 +11,29 @@
  ? Filtering vaid hex color codes in data processing.
  */
 
+    //*Method 1
 
+//  const validateHexColor = (color)=>{
+//     const regEx = /^[#0-9A-Fa-f]+$/;
+//     if(color.length===4 || color.length===7)
+//     return regEx.test(color);
+//     return false
 
- const validateHexColor = (color)=>{
-    const regEx = /^[#0-9a-f]+$/;
-    if(color.length===4 || color.length===7)
-    return regEx.test(color);
-    return false
+//  }
 
+ //*Method 2
+
+ const validateHexColor = (hexColor)=>{
+    const regEx = /^#([A-Fa-f\d]{3}|[A-fa-f\d]{6})$/
+    return regEx.test(hexColor);
  }
+
+
 
  //* Test Cases
 
  console.log(validateHexColor("#a3c113")); //O/P:true
- console.log(validateHexColor("#800080")); //O/P:true
+ console.log(validateHexColor("#fff")); //O/P:true
  console.log(validateHexColor("#1234567")); //O/P:false
  console.log(validateHexColor("a3c113")); //O/P:false
  console.log(validateHexColor("#g3c113")); //O/P:false
